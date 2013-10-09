cfdcfacd5662397aadf660d99c0cf07d72e790ff
Nila <shlikhtenkoneonila@gmail.com>
2013-10-09 10:40:35 +0300

    Added jQueru to toggle panel button

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $(function() {
+	var $panelContent = $('.j-panel-content'),
+		$openPanelArrow = $('.j-open-panel-arrow');
+	$('.j-open-panel-btn').on('click', function(e) {
+		e.preventDefault();
+		$panelContent.slideToggle();
+		$openPanelArrow.toggleClass('arrow-down').toggleClass('arrow-up');
+	})
+
+	// Add custom scroll bar to Photos
 	$('.j-photos').tinyscrollbar();
 });
